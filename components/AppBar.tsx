a6869ff6f7dda8850bff3bccf4664b3885e8f6f4
valenbenitez <benitezvalentin046@gmail.com>
2023-01-26 19:45:10 -0300

    fix footer

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,41 +1,37 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import CardMedia from "@mui/material/CardMedia";
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import BottomNavigation from '@mui/material/BottomNavigation'
+import BottomNavigationAction from '@mui/material/BottomNavigationAction'
+import RestoreIcon from '@mui/icons-material/Restore'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import CardMedia from '@mui/material/CardMedia'
 
 interface Props {
-  image: string;
+  image: string
 }
 
 export default function SimpleBottomNavigation(props: Props) {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0)
 
   return (
-    <Box sx={{ width: "100%", position: "absolute", bottom: 0 }}>
+    <Box sx={{ width: '100%', position: 'absolute', bottom: '10px' }}>
       <BottomNavigation
         showLabels
         value={value}
         onChange={(event, newValue) => {
-          setValue(newValue);
+          setValue(newValue)
         }}
-        sx={{ border: "1px solid black" }}
+        sx={{ border: '1px solid lightgray', width: '100%', borderRadius: '10px' }}
       >
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
         <BottomNavigationAction
           icon={
-            <CardMedia
-              component="img"
-              image={props.image}
-              sx={{ width: "40px", borderRadius:50, height:"40px" }}
-            />
+            <CardMedia component="img" image={props.image} sx={{ width: '60px', borderRadius: 50, height: '60px' }} />
           }
         />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
         <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
       </BottomNavigation>
     </Box>
-  );
+  )
 }
